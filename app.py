d7c23a86bce2f146c1a79dfeafcfdd378091f887
marianbobica <68694240+marianbobica@users.noreply.github.com>
2020-07-29 22:48:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-from flask import Flask, request, redirect, url_for, render_template
-
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def Login():
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/login", methods=["POST", "GET"])
+def login():
     if request.method == "POST":
-	user = request.form["nm"]
-	return redirect(url_for("user", usr=user))
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
     else:
-	return render_template("login.html")
-
+        return render_template("login.html")
 @app.route("/<usr>")
 def user(usr):
-    return f"<h1>{usr}</h1>"
-
-if __name__ == '__main__':
-    # Used when running locally only. When deploying to Cloud Run,
-    # a webserver process such as Gunicorn will serve the app.
-    app.run(host='localhost', port=8080, debug=True)
+    return "<h1>{usr}</h1>"
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
