b3c6ffb9bc232c227481a30f678b4a7506516aa1
marianbobica <68694240+marianbobica@users.noreply.github.com>
2020-07-29 21:29:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    """Return a friendly HTTP greeting."""
-    who = request.args.get('who', 'World')
-    return f'Hello {who}!\n'
+@app.route('/login', methods=['POST', 'GET'])
+def Login():
+    if request.method == "POST":
+	user = request.form["nm"]
+	return redirect(url_for("user", usr=user))
+    else:
+	return render_template("login.html")
 
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == '__main__':
     # Used when running locally only. When deploying to Cloud Run,
