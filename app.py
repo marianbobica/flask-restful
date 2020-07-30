6be126c70f496a7ff052f6e165d83c8905e25aec
marianbobica <68694240+marianbobica@users.noreply.github.com>
2020-07-30 11:41:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, redirect, url_for, render_template, request
+import os
+import json
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        fname = request.form["fname"]
+        lname = request.form["lname"]
+        user = fname + " " + lname
+        dictuser = dict(fname=fname, lname=lname)
+        return redirect(url_for("user", usr=str(json.dumps(dictuser))))
     else:
         return render_template("login.html")
-@app.route("/action_page")
-def action():
-    return render_template("action_page.php")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=5000)
